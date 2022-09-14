9c9de9285c6c905ba88afbf1544fec3258a2b18b
Victor S <victor.s.bitencourt@gmail.com>
2022-09-14 15:46:39 -0300

    feat: add authentication when creating a new product

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,18 +2,29 @@ import { Router } from 'express';
 import productsController from '../controllers/products.controller';
 import usersController from '../controllers/users.controller';
 import ordersController from '../controllers/orders.controller';
-import userAuth from '../middlewares/user.authenticate';
+import userAuthenticate from '../middlewares/user.authenticate';
+import productAuthenticate from '../middlewares/product.authenticate';
 
 const productsRouter = Router();
 const usersRouter = Router();
 const ordersRouter = Router();
 const loginRouter = Router();
 
-productsRouter.post('/products', productsController.create);
+productsRouter.post(
+  '/products',
+  productAuthenticate.validateNameField,
+  productAuthenticate.validateAmountField,
+  productsController.create,
+);
 productsRouter.get('/products', productsController.getAll);
 
 usersRouter.post('/users', usersController.create);
-loginRouter.post('/login', userAuth.validateFields, userAuth.validateUser, usersController.login);
+loginRouter.post(
+  '/login',
+  userAuthenticate.validateFields,
+  userAuthenticate.validateUser,
+  usersController.login,
+);
 
 ordersRouter.get('/orders', ordersController.getAll);
 
